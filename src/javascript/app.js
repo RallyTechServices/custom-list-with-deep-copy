f653fe650974d076ead083bdb97a8af0e257022c
Kristy Corkan <kcorkan@gmail.com>
2017-04-29 09:24:42 -0600

    fixed some messaging

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -26,6 +26,17 @@ Ext.define("custom-list-with-deep-copy", {
             scope: this
         });
         this.subscribe(this, 'statusUpdate', this.statusUpdate, this);
+        this.subscribe(this, 'maskUpdate', this.maskUpdate, this);
+        this.subscribe(this, 'copyComplete', this.copyComplete, this);
+        this.subscribe(this, 'copyError', this.copyError, this);
+    },
+    maskUpdate: function(arg){
+
+        if (!arg || arg === ''){
+            arg = false;
+        }
+        this.setLoading(arg);
+        this.refresh();
     },
     initializeApp: function(portfolioItemTypes){
         this.portfolioItemTypePaths = portfolioItemTypes;
@@ -104,7 +115,18 @@ Ext.define("custom-list-with-deep-copy", {
         }];
     },
     statusUpdate: function(msg){
-        Rally.ui.notify.Notifier.showStatus({message: msg});
+        Rally.ui.notify.Notifier.hide();
+        Rally.ui.notify.Notifier.show({message: msg, showForever: true});
+    },
+    copyComplete: function(msg){
+        this.setLoading(false);
+        Rally.ui.notify.Notifier.hide();
+        Rally.ui.notify.Notifier.show({message: msg, duration: 5000});
+    },
+    copyError: function(msg){
+        this.setLoading(false);
+        Rally.ui.notify.Notifier.hide();
+        Rally.ui.notify.Notifier.showError({message: msg, duration: 10000, saveDelay: 500});
     },
     getTypesToCopy: function(){
 
--- src/javascript/utils/menuitem-base-deepcopy.js
+++ src/javascript/utils/menuitem-base-deepcopy.js
@@ -2,6 +2,10 @@ Ext.define('Rally.ui.menu.bulk.DeepCopyBase', {
     alias: 'widget.rallyrecordmenuitembulkdeepcopybase',
     extend: 'Rally.ui.menu.bulk.MenuItem',
 
+    mixins: {
+        messageable: 'Rally.Messageable'
+    },
+
     config: {
         onBeforeAction: function(){
 //            console.log('onbeforeaction');
@@ -22,29 +26,48 @@ Ext.define('Rally.ui.menu.bulk.DeepCopyBase', {
         var me = this;
 
         this.fireEvent('loadtree');
-        Rally.ui.notify.Notifier.showStatus({message: 'Retreiving Data...'});
+        me.publish('maskUpdate', 'Retrieving Data...');
+
 
         var artifactTree = Ext.create('Rally.technicalservices.ArtifactTree',{
             portfolioItemTypes: this.portfolioItemTypes,
             listeners: {
                 treeloaded: function(tree){
-                   // me.fireEvent('loadtreecomplete');
+                    me.publish('maskUpdate', 'Copying Data...');
                     tree.deepCopy(parent);
                 },
                 copycompleted: function(rootRecord){
                    // me.fireEvent('copycomplete');
+                    me.publish('maskUpdate');
+
                     deferred.resolve({record: record});
                 },
                 copyerror: function(errorMsg){
                   //  me.fireEvent('copyerror',{record: record, errorMessage: errorMsg});
+                    me.publish('maskUpdate');
                     deferred.resolve({record: record, errorMessage: errorMsg});
                 },
                 statusupdate: function(done, total){
-                   // Rally.ui.notify.Notifier.showStatus({message:Ext.String.format("{0}: {1} of {2} Artifacts copied...", fid, done, total)});
-                    var msg = Ext.String.format("{0}: {1} of {2} Artifacts copied...", fid, done, total);
-                    this.publish('statusUpdate', msg);
-
-                    // me.fireEvent('statusupdate',msg);
+                    var divisor = 10,
+                        showMsg = false;
+                    if (total < 50){
+                        divisor = 5;
+                    }
+                    if (total < 10){
+                        divisor = 2;
+                    }
+                    if (total < 4){
+                        divisor = 1;
+                    }
+                    var showMsg = done % divisor === 0 && done > 0 && done !== total;
+                    if (showMsg){
+                        // Rally.ui.notify.Notifier.showStatus({message:Ext.String.format("{0}: {1} of {2} Artifacts copied...", fid, done, total)});
+                        var msg = Ext.String.format("{0}: {1} of {2} Artifacts copied...", fid, done, total);
+                        me.publish('statusUpdate', msg);
+                    }
+                    if (done === total){
+                        me.publish('statusUpdate', 'Stitching artifacts...');
+                    }
                 },
                 scope: this
             }
@@ -96,18 +119,20 @@ Ext.define('Rally.ui.menu.bulk.DeepCopyBase', {
             message = message + ' been deep copied';
             if (formattedID) { message += ' to ' + formattedID }
 
-            Rally.ui.notify.Notifier.show({
-                message: message
-            });
+            this.publish('copyComplete', message);
+            //Rally.ui.notify.Notifier.show({
+            //    message: message
+            //});
         } else {
             if (successfulRecords.length === 0){
                 message = "0 items have been deep copied";
             }
 
-            Rally.ui.notify.Notifier.showError({
-                message: message + ', but ' + unsuccessfulRecords.length + ' failed: ' + errorMessage,
-                saveDelay: 500
-            });
+            this.publish('copyError', message + ', but ' + unsuccessfulRecords.length + ' failed: ' + errorMessage);
+            //Rally.ui.notify.Notifier.showError({
+            //    message: message + ', but ' + unsuccessfulRecords.length + ' failed: ' + errorMessage,
+            //    saveDelay: 500
+            //});
         }
 
         Ext.callback(this.onActionComplete, null, [successfulRecords, unsuccessfulRecords]);
